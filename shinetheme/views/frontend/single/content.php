000a8a7ef2ede0455cf1ae1eaeb13fb81e30bcd2
quandq <quandq@shinetheme.com>
2016-04-07 11:47:48 +0700

    update single room

--- shinetheme/views/frontend/single/content.php
+++ shinetheme/views/frontend/single/content.php
@@ -14,7 +14,7 @@ $service_type = get_post_meta(get_the_ID(),'service_type',true);
 <div  itemscope itemtype="http://schema.org/Product" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<meta itemprop="url" content="<?php the_permalink(); ?>" />
-    <div class="container traveler-single-content">
+    <div class="container-fluid traveler-single-content">
         <div class="row">
             <div class="col-md-2">
                 <?php if(has_post_thumbnail() and get_the_post_thumbnail()){
@@ -84,8 +84,9 @@ $service_type = get_post_meta(get_the_ID(),'service_type',true);
                     $map_lat = get_post_meta( get_the_ID() , 'map_lat', true );
                     $map_lng = get_post_meta( get_the_ID() , 'map_long', true );
                     $map_zoom = get_post_meta( get_the_ID() , 'map_zoom', true );
-                    ?>
-                    <div class="traveler_google_map" data-lat="<?php echo esc_attr($map_lat) ?>" data-lng="<?php echo esc_attr($map_lng) ?>" data-zoom="<?php echo esc_attr($map_zoom) ?>"></div>
+                    if(!empty($map_lat) and !empty($map_lng)){ ?>
+                        <div class="traveler_google_map" data-lat="<?php echo esc_attr($map_lat) ?>" data-lng="<?php echo esc_attr($map_lng) ?>" data-zoom="<?php echo esc_attr($map_zoom) ?>"></div>
+                    <?php } ?>
                 </div>
             </div>
         </div>
