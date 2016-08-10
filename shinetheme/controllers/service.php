c713c81014832830b5c89b153ccf9a0533452bb3
dannie.haui <dungdt@shinetheme.com>
2016-08-10 18:14:11 +0700

    Add Review title

--- shinetheme/controllers/service.php
+++ shinetheme/controllers/service.php
@@ -488,8 +488,9 @@ if (!class_exists('WPBooking_Service_Controller')) {
 
 			if ($validate) {
 
-				update_comment_meta($comment_id, 'wpbooking_review', WPBooking_Input::post('wpbooking_review'));
-				update_comment_meta($comment_id, 'wpbooking_review_detail', WPBooking_Input::post('wpbooking_review_detail'));
+				update_comment_meta($comment_id, 'wpbooking_review', $this->post('wpbooking_review'));
+				update_comment_meta($comment_id, 'wpbooking_review_detail',$this->post('wpbooking_review_detail'));
+				update_comment_meta($comment_id,'wpbooking_title',$this->post('wpbooking_title'));
 			}
 
 			do_action('after_wpbooking_update_review_stats', $validate, $comment_id, $post_id);
