fb6b6b49df42b1dc37a8cfb029759618a0ced1f0
dannie.haui <dungdt@shinetheme.com>
2016-08-15 10:04:09 +0700

    Apply mini cart style

--- shinetheme/views/frontend/cart/cart-widget.php
+++ shinetheme/views/frontend/cart/cart-widget.php
@@ -6,48 +6,39 @@
  * Time: 5:15 PM
  */
 $booking=WPBooking_Order::inst();
-$items=$booking->get_cart();
+$cart=$booking->get_cart();
 ?>
 <div class="wpbooking-cart-widget-content">
 <?php
-if(empty($items)){
+if(empty($cart)){
 	printf('<p class="alert alert-warning">%s</p>',__('Your cart is currently empty','wpbooking'));
 }else{
-	?>
-		<ul class="cart-widget-items">
-			<?php
-			foreach($items as $key=>$value){
-				$service_type=$value['service_type'];
-				?>
-				<li>
-					<div class="cart-item-img">
-						<?php echo get_the_post_thumbnail($value['post_id'],array(90,90)) ?>
-					</div>
-					<div class="cart-item-content">
-						<h5 class="service-title"><a href="<?php echo get_permalink($value['post_id']) ?>" target="_blank" ><?php echo get_the_title($value['post_id']) ?></a></h5>
-						<span class="price">
-							<?php echo $booking->get_cart_item_total_html($value) ?>
-						</span>
-						<div class="service-meta">
-							<?php do_action('wpbooking_cart_item_information',$value) ?>
-							<?php do_action('wpbooking_cart_item_information_'.$service_type,$value) ?>
-						</div>
-					</div>
-				</li>
-				<?php
-			}
-
-			?>
-		</ul>
-		<hr>
-		<div class="cart-widget-total">
-			<p class="total"><?php _e('Total:','wpbooking') ?> <?php echo $booking->get_cart_total() ?></p>
+	foreach($cart as $key=>$value)
+	{
+		$service_type=$value['service_type'];
+		$post_id=$value['post_id'];
+		$service=new WB_Service($value['post_id']);
+		$featured=$service->get_featured_image();
+		?>
+		<div class="review-order-item">
+			<div class="review-order-item-img">
+				<a href="<?php echo get_permalink($post_id)?>" target="_blank"><?php echo wp_kses($featured['thumb'],array('img'=>array('src'=>array(),'alt'=>array())))?></a>
+			</div>
+			<div class="review-order-item-info">
+				<h4 class="service-name"><a href="<?php echo get_permalink($value['post_id'])?>" target="_blank"><?php echo get_the_title($value['post_id'])?></a></h4>
+				<?php if($address=$service->get_address()){
+					printf('<p class="service-address">%s</p>',$address);
+				} ?>
+				<p class="review-order-item-price"><?php echo ($booking->get_cart_item_total_html($value)) ?></p>
+				<?php do_action('wpbooking_review_order_item_information',$value) ?>
+				<?php do_action('wpbooking_review_order_item_information_'.$service_type,$value) ?>
+			</div>
+			<a class="delete-cart-item" onclick="return confirm('<?php esc_html_e('Do you want to delete it?','wpbooking') ?>')" href="<?php echo esc_url(add_query_arg(array('delete_cart_item'=>$key),$booking->get_checkout_url())) ?>">
+				<i class="fa fa-times"></i>
+			</a>
 		</div>
-
-		<div class="cart-widget-actions">
-			<a href="<?php echo esc_url($booking->get_checkout_url()) ?>" class="btn btn-success"><?php _e('Checkout Now','wpbooking') ?></a>
-		</div>
-	<?php
+		<?php
+	}
 }
 ?>
 </div>
