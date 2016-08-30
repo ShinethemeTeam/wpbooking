f3657840c10cbbe1bae10a0582b04ed27395e787
dannie.haui <dungdt@shinetheme.com>
2016-08-30 11:07:48 +0700

    Test checkout form data

--- shinetheme/views/admin/order/detail.php
+++ shinetheme/views/admin/order/detail.php
@@ -10,6 +10,7 @@ $booking=WPBooking_Order::inst();
 $value=$order_item;
 $order=new WB_Order($order_id);
 $checkout_form_data=$order->get_checkout_form_data();
+var_dump($checkout_form_data);
 ?>
 
 <div class="wrap">
