cddd8be7a16854006696ce49d566b56949015a20
quandq@shinetheme.com <quandq>
2016-09-28 14:54:40 +0700

    update meta

--- shinetheme/libraries/service-types/hotel.php
+++ shinetheme/libraries/service-types/hotel.php
@@ -350,68 +350,7 @@ if (!class_exists('WPBooking_Hotel_Service_Type') and class_exists('WPBooking_Ab
                         ),
                     )
                 ),
-                'facilities_tab'=>array(
-                    'label' => __('4. Facilities', 'wpbooking'),
-                    'fields'=>array(
-                        array( 'type'  => 'open_section'),
-                        array(
-                            'label' => __("Extra bed optional", 'wpbooking'),
-                            'type'  => 'title',
-                            'desc' =>esc_html__("These are the bed options that can be added upon request.","wpbooking")
-                        ),
-                        array(
-                            'label' => __('Can you provide extra beds?', 'wpbooking'),
-                            'id'    => 'extra_bed',
-                            'type'  => 'radio',
-                            'value' => array(
-                                "yes"=>esc_html__("Yes",'wpbooking'),
-                                "no"=>esc_html__("No",'wpbooking'),
-                            ),
-                            'class' => 'radio_pro',
-                            'desc' =>esc_html__("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium","wpbooking")
-                        ),
-                        array(
-                            'label' => __('Select the number of extra beds that can be added.', 'wpbooking'),
-                            'id'    => 'double_bed',
-                            'type'  => 'dropdown',
-                            'value' => array(
-                                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
-                            ),
-                            'class' => 'small',
-                            'desc' =>esc_html__("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium","wpbooking")
-                        ),
-                        array(
-                            'label'       => __("Price for each extra beds", 'wpbooking'),
-                            'type'        => 'money_input',
-                            'id'          => 'price_for_extra_bed',
-                            'class'       => 'small',
-                            'std'           => '0',
-                            'desc' => esc_html__('Example: 2 extra bed and price is 10.00, total cost is 20.00', 'wpbooking')
-                        ),
-                        array( 'type'  => 'close_section'),
-                        array( 'type'  => 'open_section'),
-                        array(
-                            'label' => __("Space", 'wpbooking'),
-                            'type'  => 'title',
-                            'desc' =>esc_html__("We display your room size to guests on your Booking.com propert","wpbooking")
-                        ),
-                        array(
-                            'label' => __('What is your preferred  unit of measure?', 'wpbooking'),
-                            'id'    => 'room_measunit',
-                            'type'  => 'radio',
-                            'value' => array(
-                                "metres"=>esc_html__("Square metres",'wpbooking'),
-                                "feed"=>esc_html__("Square feet",'wpbooking'),
-                            ),
-                            'class' => 'radio_pro',
-                            'desc' =>esc_html__("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium","wpbooking")
-                        ),
-                        array( 'type'  => 'close_section'),
-                        array(
-                            'type' => 'section_navigation',
-                        ),
-                    )
-                ),
+
                 'policies_tab' => array(
                     'label'  => __('3. Policies', 'wpbooking'),
                     'fields' => array(
@@ -499,8 +438,75 @@ if (!class_exists('WPBooking_Hotel_Service_Type') and class_exists('WPBooking_Ab
                         ),
                     )
                 ),
+                'facilities_tab'=>array(
+                    'label' => __('4. Facilities', 'wpbooking'),
+                    'fields'=>array(
+                        array( 'type'  => 'open_section'),
+                        array(
+                            'label' => __("Extra bed optional", 'wpbooking'),
+                            'type'  => 'title',
+                            'desc' =>esc_html__("These are the bed options that can be added upon request.","wpbooking")
+                        ),
+                        array(
+                            'label' => __('Can you provide extra beds?', 'wpbooking'),
+                            'id'    => 'extra_bed',
+                            'type'  => 'radio',
+                            'value' => array(
+                                "yes"=>esc_html__("Yes",'wpbooking'),
+                                "no"=>esc_html__("No",'wpbooking'),
+                            ),
+                            'class' => 'radio_pro',
+                            'desc' =>esc_html__("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium","wpbooking")
+                        ),
+                        array(
+                            'label' => __('Select the number of extra beds that can be added.', 'wpbooking'),
+                            'id'    => 'double_bed',
+                            'type'  => 'dropdown',
+                            'value' => array(
+                                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+                            ),
+                            'class' => 'small',
+                            'desc' =>esc_html__("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium","wpbooking")
+                        ),
+                        array(
+                            'label'       => __("Price for each extra beds", 'wpbooking'),
+                            'type'        => 'money_input',
+                            'id'          => 'price_for_extra_bed',
+                            'class'       => 'small',
+                            'std'           => '0',
+                            'desc' => esc_html__('Example: 2 extra bed and price is 10.00, total cost is 20.00', 'wpbooking')
+                        ),
+                        array( 'type'  => 'close_section'),
+                        array( 'type'  => 'open_section'),
+                        array(
+                            'label' => __("Space", 'wpbooking'),
+                            'type'  => 'title',
+                            'desc' =>esc_html__("We display your room size to guests on your Booking.com propert","wpbooking")
+                        ),
+                        array(
+                            'label' => __('What is your preferred  unit of measure?', 'wpbooking'),
+                            'id'    => 'room_measunit',
+                            'type'  => 'radio',
+                            'value' => array(
+                                "metres"=>esc_html__("Square metres",'wpbooking'),
+                                "feed"=>esc_html__("Square feet",'wpbooking'),
+                            ),
+                            'class' => 'radio_pro',
+                            'desc' =>esc_html__("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium","wpbooking")
+                        ),
+                        array(
+                            'label' => __('Room sizes', 'wpbooking'),
+                            'id'    => 'room_size',
+                            'type'  => 'room_size',
+                        ),
+                        array( 'type'  => 'close_section'),
+                        array(
+                            'type' => 'section_navigation',
+                        ),
+                    )
+                ),
                 'photo_tab'    => array(
-                    'label'  => __('4. Photos /', 'wpbooking'),
+                    'label'  => __('5. Photos', 'wpbooking'),
                     'fields' => array(
                         array(
                             'label' => __("Pictures", 'wpbooking'),
