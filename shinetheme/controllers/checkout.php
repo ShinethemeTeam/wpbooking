51f5f1c4916b15a1b8cf27b65c4650f74d94cab1
Dannie <Dannie@Dungdt-pc.local.com>
2016-10-18 17:10:01 +0700

    Add class checkout

---
+++ shinetheme/controllers/checkout.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dungdt
+ * Date: 10/18/2016
+ * Time: 9:33 AM
+ */
+if(!class_exists('WPBooking_Checkout_Controller'))
+{
+    class WPBooking_Checkout_Controller extends WPBooking_Controller
+    {
+        function __construct()
+        {
+            parent::__construct();
+        }
+
+
+    }
+}
