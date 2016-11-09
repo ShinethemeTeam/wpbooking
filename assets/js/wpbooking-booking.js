89f392de4961b496ff5258401af1803815c8fec5
Dannie <Dannie@Dungdt-pc.local.com>
2016-11-09 11:03:15 +0700

    Fix Date Object Picker

--- assets/js/wpbooking-booking.js
+++ assets/js/wpbooking-booking.js
@@ -378,7 +378,7 @@ jQuery(document).ready(function($){
             minDate:0,
             onSelect:function(selected) {
                 var p = $(this).parent();
-                var check_in = new Date(selected);
+                var check_in = $(this).datepicker( 'getDate' );
                 p.find('.checkin_d').val(check_in.getDate());
                 p.find('.checkin_m').val(check_in.getMonth()+1);
                 p.find('.checkin_y').val(check_in.getFullYear());
