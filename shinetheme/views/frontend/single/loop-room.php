7e2c35ce0498b538d6f2fda0473875d9de751202
quandq@shinetheme.com <quandq>
2016-11-17 14:59:21 +0700

    udate single

--- shinetheme/views/frontend/single/loop-room.php
+++ shinetheme/views/frontend/single/loop-room.php
@@ -277,7 +277,7 @@ $diff = $diff / (60 * 60 * 24);
                             echo '<div class="title">'.$rental_features->labels->name.'</div>';
                             foreach($term_ids as $key=>$value){
                                 $term = get_term($value,$taxonomy);
-                                if(!is_wp_error($term)){
+                                if(!is_wp_error($term) and !empty($term->name)){
                                 ?>
                                 <div class="item col-33">
                                     <input type="checkbox" checked onclick="return false">
