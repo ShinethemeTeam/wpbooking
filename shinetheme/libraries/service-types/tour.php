e61724a06728b5b88b5195250316e8f92a69ff30
Dannie <Dannie@Dungdt-pc.local.com>
2016-12-13 10:41:05 +0700

    Fix Order By

--- shinetheme/libraries/service-types/tour.php
+++ shinetheme/libraries/service-types/tour.php
@@ -365,7 +365,7 @@ if (!class_exists('WPBooking_Tour_Service_Type') and class_exists('WPBooking_Abs
             switch ($service->get_meta('pricing_type')) {
                 case "per_unit":
                     $query = $calendar->select($wpdb->prefix . 'wpbooking_availability.id,
-	' . $wpdb->prefix . 'wpbooking_service.max_guests,calendar_maximum,(adult_number + children_number + infant_number) AS total_people_booked,start,calendar_price')
+	' . $wpdb->prefix . 'wpbooking_service.max_guests,calendar_minimum,calendar_maximum,(adult_number + children_number + infant_number) AS total_people_booked,start,calendar_price')
                         ->join('wpbooking_service', "wpbooking_service.post_id = wpbooking_availability.post_id")
                         ->join('wpbooking_order', "wpbooking_order.post_id = wpbooking_availability.post_id and wpbooking_order. STATUS NOT IN ('cancelled','refunded','trash','payment_failed')", 'left')
                         ->where(array(
@@ -393,9 +393,8 @@ if (!class_exists('WPBooking_Tour_Service_Type') and class_exists('WPBooking_Abs
                                 wpbooking_set_message(sprintf(esc_html__('This tour only remain availability for %d people', 'wpbooking'), $query['max_guests'] - $query['total_people_booked']), 'error');
                             }
                             // Check Max, Min
-                            $min = $query['calendar_minimum'];
-                            $max = $query['calendar_maximum'];
-
+                            $min = (int)$query['calendar_minimum'];
+                            $max = (int)$query['calendar_maximum'];
                             if ($min <= $max) {
                                 if ($min) {
                                     if ($total_people < $min) {
@@ -1053,6 +1052,7 @@ if (!class_exists('WPBooking_Tour_Service_Type') and class_exists('WPBooking_Abs
                             'end <='                                         => $end,
                         ))
                         ->groupby($wpdb->prefix . 'wpbooking_availability.id')
+                        ->orderby($wpdb->prefix . 'wpbooking_availability.start')
                         ->having(' total_people_booked IS NULL OR total_people_booked < max_guests')
                         ->get()->result();
                     $calendar->_clear_query();
@@ -1074,6 +1074,7 @@ if (!class_exists('WPBooking_Tour_Service_Type') and class_exists('WPBooking_Abs
                         ))
                         ->where("({$wpdb->prefix}wpbooking_availability.adult_price > 0 or {$wpdb->prefix}wpbooking_availability.child_price>0 or {$wpdb->prefix}wpbooking_availability.infant_price>0)", false, true)
                         ->groupby($wpdb->prefix . 'wpbooking_availability.id')
+                        ->orderby($wpdb->prefix . 'wpbooking_availability.start')
                         ->having(' total_people_booked IS NULL OR total_people_booked < max_guests')
                         ->get()->result();
                 default:
