5932199852ecd67f51dcf8036f1aa0fd24f1ce98
quandq@shinetheme.com <quandq>
2017-03-01 11:11:13 +0700

    update

--- shinetheme/libraries/service-types/accommodation.php
+++ shinetheme/libraries/service-types/accommodation.php
@@ -791,7 +791,7 @@ if (!class_exists('WPBooking_Accommodation_Service_Type') and class_exists('WPBo
                                 'vat_unit',
                             ),
                             'extra_rules'=>array(
-                                'vat_amount'=>array('label'=>esc_html__('VAT amount','wpbooking'),'rules'=>'required|decimal|greater_than[0]','rule_condition'=>'vat_excluded:not_empty')
+                                'vat_amount'=>array('label'=>esc_html__('VAT amount','wpbooking'),'rules'=>'required|greater_than[0]','rule_condition'=>'vat_excluded:not_empty')
                             ),
 
                         ),
@@ -805,7 +805,7 @@ if (!class_exists('WPBooking_Accommodation_Service_Type') and class_exists('WPBo
                                 'citytax_unit',
                             ),
                             'extra_rules'=>array(
-                                'citytax_amount'=>array('label'=>esc_html__('City Tax amount','wpbooking'),'rules'=>'required|decimal|greater_than[0]','rule_condition'=>'citytax_excluded:not_empty')
+                                'citytax_amount'=>array('label'=>esc_html__('City Tax amount','wpbooking'),'rules'=>'required|greater_than[0]','rule_condition'=>'citytax_excluded:not_empty')
                             ),
                         ),
 
