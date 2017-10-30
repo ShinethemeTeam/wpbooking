fc07e4111e4e935e3170643fae3d8233879ed4f2
ADMINISTRATOR\Administrator <haint@shinetheme.com>
2017-10-30 08:26:41 +0700

    update comment, calendar

--- shinetheme/models/calendar_model.php
+++ shinetheme/models/calendar_model.php
@@ -1,99 +1,101 @@
 <?php
-/**
- * @since 1.0.0
- **/
+    /**
+     * @since 1.0.0
+     **/
 
-if (!defined('ABSPATH')) {
-	exit; // Exit if accessed directly
-}
-if (!class_exists('WPBooking_Calendar_Model')) {
-	class WPBooking_Calendar_Model extends WPBooking_Model
-	{
+    if ( !defined( 'ABSPATH' ) ) {
+        exit; // Exit if accessed directly
+    }
+    if ( !class_exists( 'WPBooking_Calendar_Model' ) ) {
+        class WPBooking_Calendar_Model extends WPBooking_Model
+        {
 
-		static $_inst = FALSE;
+            static $_inst = false;
 
 
-		public function __construct()
-		{
-			$this->table_version = '1.0.6';
-			$this->table_name = 'wpbooking_availability';
-			$this->columns = array(
-				'id'            => array(
-					'type'           => 'int',
-					'AUTO_INCREMENT' => TRUE
-				),
-				'post_id'       => array('type' => 'int', 'length' => 11),
-				'start'         => array('type' => 'int' ),
-				'end'           => array('type' => 'int'),
-				'price'         => array('type' => 'varchar','length' => 255),
-				'calendar_minimum'  => array('type' => 'int', 'length' => 4),
-				'calendar_maximum'    => array('type' => 'int', 'length' => 4),
-				'calendar_price'    => array('type' => 'FLOAT', 'length' => 255),
-				'adult_minimum'    => array('type' => 'int', 'length' => 4),
-				'adult_price'    => array('type' => 'FLOAT', 'length' => 255),
-                'child_minimum'    => array('type' => 'int', 'length' => 4),
-                'child_price'    => array('type' => 'FLOAT', 'length' => 255),
-                'infant_minimum'    => array('type' => 'int', 'length' => 4),
-                'infant_price'    => array('type' => 'FLOAT', 'length' => 255),
-				'weekly'        => array('type' => 'FLOAT'),
-				'monthly'       => array('type' => 'FLOAT'),
-				'status'        => array('type' => 'varchar', 'length' => 255),
-				'base_id'       => array('type' => 'int', 'length' => 11),
-				'can_check_in'  => array('type' => 'int', 'length' => 4),
-				'can_check_out' => array('type' => 'int', 'length' => 4),
-				'group_day'     => array('type' => 'varchar', 'length' => 255),
+            public function __construct()
+            {
+                $this->table_version = '1.0.6';
+                $this->table_name    = 'wpbooking_availability';
+                $this->columns       = [
+                    'id'               => [
+                        'type'           => 'int',
+                        'AUTO_INCREMENT' => true
+                    ],
+                    'post_id'          => [ 'type' => 'int', 'length' => 11 ],
+                    'start'            => [ 'type' => 'int' ],
+                    'end'              => [ 'type' => 'int' ],
+                    'price'            => [ 'type' => 'varchar', 'length' => 255 ],
+                    'calendar_minimum' => [ 'type' => 'int', 'length' => 4 ],
+                    'calendar_maximum' => [ 'type' => 'int', 'length' => 4 ],
+                    'calendar_price'   => [ 'type' => 'FLOAT', 'length' => 255 ],
+                    'adult_minimum'    => [ 'type' => 'int', 'length' => 4 ],
+                    'adult_price'      => [ 'type' => 'FLOAT', 'length' => 255 ],
+                    'child_minimum'    => [ 'type' => 'int', 'length' => 4 ],
+                    'child_price'      => [ 'type' => 'FLOAT', 'length' => 255 ],
+                    'infant_minimum'   => [ 'type' => 'int', 'length' => 4 ],
+                    'infant_price'     => [ 'type' => 'FLOAT', 'length' => 255 ],
+                    'weekly'           => [ 'type' => 'FLOAT' ],
+                    'monthly'          => [ 'type' => 'FLOAT' ],
+                    'status'           => [ 'type' => 'varchar', 'length' => 255 ],
+                    'base_id'          => [ 'type' => 'int', 'length' => 11 ],
+                    'can_check_in'     => [ 'type' => 'int', 'length' => 4 ],
+                    'can_check_out'    => [ 'type' => 'int', 'length' => 4 ],
+                    'group_day'        => [ 'type' => 'varchar', 'length' => 255 ],
 
-			);
-			parent::__construct();
-			
-		}
+                ];
+                parent::__construct();
 
-		function get_prices($post_id, $from, $to)
-		{
-			return $this->where('post_id', $post_id)->where('start>=', $from)->where('start<=', $to)->get()->result();
-		}
+            }
 
-		/**
-		 * Get Calendar Data From Date To Date
-		 *
-		 * @since 1.0
-		 * @author dungdt
-		 *
-		 * @param $post_id
-		 * @param $start_date string timepstamp
-		 * @param $end_date string timepstamp
-		 * @return array|bool
-		 */
-		function calendar_months($post_id, $start_date, $end_date)
-		{
-			global $wpdb;
-			$today = strtotime('today');
-			if ($start_date < $today) $start_date = $today;
-			$res = $this
-				->select(array(
-					$wpdb->prefix . 'wpbooking_availability.*',
-				))
-				->join('posts', 'posts.ID=wpbooking_availability.post_id')
-				->where(array(
-					$wpdb->prefix . 'wpbooking_availability.post_id' => $post_id,
-					'start>='                                        => $start_date,
-					'end<='                                          => $end_date
-				))
-				->groupby($wpdb->prefix . 'wpbooking_availability.id')
-				->orderby('start', 'asc')->get()->result();
-			return $res;
-		}
+            function get_prices( $post_id, $from, $to )
+            {
+                return $this->where( 'post_id', $post_id )->where( 'start>=', $from )->where( 'start<=', $to )->get()->result();
+            }
 
-		static function inst()
-		{
-			if (!self::$_inst) {
-				self::$_inst = new self();
-			}
+            /**
+             * Get Calendar Data From Date To Date
+             *
+             * @since  1.0
+             * @author dungdt
+             *
+             * @param $post_id
+             * @param $start_date string timepstamp
+             * @param $end_date   string timepstamp
+             *
+             * @return array|bool
+             */
+            function calendar_months( $post_id, $start_date, $end_date )
+            {
+                global $wpdb;
+                $today = strtotime( 'today' );
+                if ( $start_date < $today ) $start_date = $today;
+                $res = $this
+                    ->select( [
+                        $wpdb->prefix . 'wpbooking_availability.*',
+                    ] )
+                    ->join( 'posts', 'posts.ID=wpbooking_availability.post_id' )
+                    ->where( [
+                        $wpdb->prefix . 'wpbooking_availability.post_id' => $post_id,
+                        'start>='                                        => $start_date,
+                        'end<='                                          => $end_date
+                    ] )
+                    ->groupby( $wpdb->prefix . 'wpbooking_availability.id' )
+                    ->orderby( 'start', 'asc' )->get()->result();
 
-			return self::$_inst;
-		}
+                return $res;
+            }
 
-	}
+            static function inst()
+            {
+                if ( !self::$_inst ) {
+                    self::$_inst = new self();
+                }
 
-	WPBooking_Calendar_Model::inst();
-}
+                return self::$_inst;
+            }
+
+        }
+
+        WPBooking_Calendar_Model::inst();
+    }
--- shinetheme/models/comments_model.php
+++ shinetheme/models/comments_model.php
@@ -1,80 +1,85 @@
 <?php
-if(!class_exists('WPBooking_Comment_Model') && class_exists('WPBooking_Model'))
-{
-	class WPBooking_Comment_Model extends WPBooking_Model
-	{
-		static $_inst;
-
-		function __construct()
-		{
-			$this->table_name='comments';
-			$this->ignore_create_table=TRUE;
-			parent::__construct();
-		}
-
-
-        /**
-         * Get Avg Rate for Specific Post
-         *
-         * @since 1.0
-         * @author dungdt
-         *
-         * @param bool|FALSE $post_id
-         * @return bool
-         */
-        function get_avg_review($post_id=FALSE)
+    if ( !class_exists( 'WPBooking_Comment_Model' ) && class_exists( 'WPBooking_Model' ) ) {
+        class WPBooking_Comment_Model extends WPBooking_Model
         {
-            global $wpdb;
-            if(!$post_id) return FALSE;
+            static $_inst;
 
-            $row=$this->select('avg('.$wpdb->commentmeta.'.meta_value) as avg_rate')
-                ->join('commentmeta','commentmeta.comment_id=comments.comment_ID')
-                ->where($wpdb->commentmeta.'.meta_key','wb_review')
-                ->where('comment_post_ID',$post_id)
-                ->where('comment_approved',1)
-                ->get()->row();
+            function __construct()
+            {
+                $this->table_name          = 'comments';
+                $this->ignore_create_table = true;
+                parent::__construct();
+            }
 
-            return !empty($row['avg_rate'])?$row['avg_rate']:FALSE;
+
+            /**
+             * Get Avg Rate for Specific Post
+             *
+             * @since  1.0
+             * @author dungdt
+             *
+             * @param bool|FALSE $post_id
+             *
+             * @return bool
+             */
+            function get_avg_review( $post_id = false )
+            {
+                global $wpdb;
+                if ( !$post_id ) return false;
+
+                $row = $this->select( 'avg(' . $wpdb->commentmeta . '.meta_value) as avg_rate' )
+                    ->join( 'commentmeta', 'commentmeta.comment_id=comments.comment_ID' )
+                    ->where( $wpdb->commentmeta . '.meta_key', 'wb_review' )
+                    ->where( 'comment_post_ID', $post_id )
+                    ->where( 'comment_approved', 1 )
+                    ->get()->row();
+
+                return !empty( $row[ 'avg_rate' ] ) ? $row[ 'avg_rate' ] : false;
+            }
+
+            /**
+             * Get Number of Reply for a Review
+             *
+             * @author dungdt
+             * @since  1.0
+             *
+             * @param $review_id
+             *
+             * @return mixed
+             */
+            function count_child( $review_id )
+            {
+
+                $res = $this->select( 'count(comment_ID) as total' )->where( 'comment_parent', $review_id )->get( 1 )->row();
+
+                return $res[ 'total' ];
+            }
+
+            function count_parent( $post_id = false )
+            {
+
+                if ( !$post_id ) return false;
+
+                $row = $this->select( 'count(comment_ID) as total' )
+                    ->where( [
+                        'comment_post_ID'  => $post_id,
+                        'comment_parent'   => 0,
+                        'comment_approved' => 1
+                    ] )->get()->row();
+
+                return !empty( $row[ 'total' ] ) ? $row[ 'total' ] : false;
+            }
+
+            static function inst()
+            {
+                if ( !self::$_inst ) self::$_inst = new self();
+
+                return self::$_inst;
+            }
+
+
         }
 
-        /**
-         * Get Number of Reply for a Review
-         *
-         * @author dungdt
-         * @since 1.0
-         *
-         * @param $review_id
-         * @return mixed
-         */
-        function count_child($review_id){
+        WPBooking_Comment_Model::inst();
 
-            $res=$this->select('count(comment_ID) as total')->where('comment_parent',$review_id)->get(1)->row();
-
-            return $res['total'];
-        }
-
-        function count_parent($post_id = false){
-
-            if(!$post_id) return FALSE;
-
-            $row=$this->select('count(comment_ID) as total')
-                ->where(array(
-                    'comment_post_ID'=>$post_id,
-                    'comment_parent'=>0,
-                    'comment_approved'=>1
-                ))->get()->row();
-            return !empty($row['total'])?$row['total']:FALSE;
-        }
-
-		static function inst()
-		{
-			if(!self::$_inst) self::$_inst=new self();
-			return self::$_inst;
-		}
-
-
-	}
-
-    WPBooking_Comment_Model::inst();
-
-}
+    }
