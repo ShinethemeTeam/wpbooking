703741d1bf997f6fc2ec782f996aac5308ac8d49
ADMINISTRATOR\Administrator <haint@shinetheme.com>
2018-07-28 08:09:22 +0700

    z

--- shinetheme/libraries/shortcodes/emails/order_name_customer.php
+++ shinetheme/libraries/shortcodes/emails/order_name_customer.php
@@ -6,7 +6,7 @@ if(!function_exists('wpbooking_email_name_customer_func'))
 		$order_id=WPBooking()->get('order_id');
 		$f_name=get_post_meta($order_id,'wpbooking_user_first_name',true);
         $l_name=get_post_meta($order_id,'wpbooking_user_last_name',true);
-        $full = $f_name.$l_name;
+        $full = $f_name.' '.$l_name;
 		if(empty($full)){
 			return 'Jonathan & Leo';
 		}
